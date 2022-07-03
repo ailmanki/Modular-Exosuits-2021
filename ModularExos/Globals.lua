21b674a227c4a5de1b2adc19b2119d8b6dce0e57
ailmanki <1570625+ailmanki@users.noreply.github.com>
2022-07-03 02:50:17 +0200

    fix missing ExoShield

--- ModularExos/Globals.lua
+++ ModularExos/Globals.lua
@@ -2,7 +2,7 @@ local oldGetPrecachedCosmeticMaterial = GetPrecachedCosmeticMaterial
 function GetPrecachedCosmeticMaterial( className, variantId, viewOnly )
     if className == "Claw"  then
 		className = "Minigun"
-	elseif (className == "ExoWelder") or (className ==  "ExoFlamer") then
+	elseif (className == "ExoWelder") or (className == "ExoFlamer") or (className == "ExoShield")then
 		className = "Railgun"
 	end
 
